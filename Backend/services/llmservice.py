4a62a26123c01192644e597253fa87345296f24b
harshitp1603 <harshitp1603@gmail.com>
2026-06-03 01:25:00 +0530

    Switch LLM from ChatOpenAI to ChatGoogleGenerativeAI

--- Backend/services/llmservice.py
+++ Backend/services/llmservice.py
@@ -1,14 +1,12 @@
 from dotenv import load_dotenv
-import os
-
 load_dotenv()
 
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import (
+    ChatGoogleGenerativeAI
+)
 
-llm = ChatOpenAI(
-    model="openai/gpt-4o-mini",
-    api_key=os.getenv("OPENROUTER_API_KEY"),
-    base_url="https://openrouter.ai/api/v1",
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
     temperature=0,
     streaming=True
 )
